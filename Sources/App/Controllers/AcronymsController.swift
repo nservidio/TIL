625b5319645fedc5cddc0833c40c9e7b11d29626
Nick Servidio <servidionicholas@gmail.com>
2018-12-12 15:00:49 -0500

    Add parent-child relationship

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -1,5 +1,6 @@
 import Vapor
 import Fluent
+
 struct AcronymsController: RouteCollection {
     func boot(router: Router) throws {
         let acronymsRoutes = router.grouped("api", "acronyms")
@@ -11,6 +12,9 @@ struct AcronymsController: RouteCollection {
         acronymsRoutes.get("search", use: searchHandler)
         acronymsRoutes.get("first", use: getFirstHandler)
         acronymsRoutes.get("sorted", use: sortedHandler)
+        acronymsRoutes.get(
+            Acronym.parameter, "user",
+            use: getUserHandler)
     }
 
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -36,9 +40,9 @@ struct AcronymsController: RouteCollection {
         ) { acronym, updatedAcronym in
             acronym.short = updatedAcronym.short
             acronym.long = updatedAcronym.long
+            acronym.userID = updatedAcronym.userID
             return acronym.save(on: req)
         }
-
     }
 
     func deleteHandler(_ req: Request)
@@ -74,6 +78,16 @@ struct AcronymsController: RouteCollection {
 
     func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+
+    func getUserHandler(_ req: Request) throws -> Future<User> {
+        // 2
+        return try req
+            .parameters.next(Acronym.self)
+            .flatMap(to: User.self) { acronym in
+                // 3
+                acronym.user.get(on: req)
+        }
     }
 }
 
--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -3,15 +3,40 @@ import FluentPostgreSQL
 
 final class Acronym: Codable {
     var id: Int?
+    var userID: User.ID
     var short: String
     var long: String
-    init(short: String, long: String) {
+    init(short: String, long: String, userID: User.ID) {
         self.short = short
         self.long = long
+        self.userID = userID
     }
 }
 
 extension Acronym: PostgreSQLModel {}
-extension Acronym: Migration {}
+
+// 1
+extension Acronym: Migration {
+    // 2
+    static func prepare(
+        on connection: PostgreSQLConnection
+        ) -> Future<Void> {
+        // 3
+        return Database.create(self, on: connection) { builder in
+            // 4
+            try addProperties(to: builder)
+            // 5
+            builder.reference(from: \.userID, to: \User.id)
+        } }
+}
+
 extension Acronym: Content {}
 extension Acronym: Parameter {}
+
+extension Acronym {
+    // 1
+    var user: Parent<Acronym, User> {
+        // 2
+        return parent(\.userID)
+    }
+}
